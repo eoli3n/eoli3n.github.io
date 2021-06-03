790df0999d52f8d6356f739e8b674c66bf664163
eoli3n <jonathan.kirszling@runbox.com>
2021-06-03 23:32:44 +0200

    auto heading links

---
+++ assets/js/headings.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.h2,h3,h4,h5,h6').filter('[id]').each(function () {
+        $(this).html('<a href="#'+$(this).attr('id')+'">' + $(this).text() + '</a>');
+    });
+});
